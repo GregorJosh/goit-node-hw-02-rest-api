4e4fef9901c36142d85390ed4bdd8e0ad560a69f
GregorJosh <grzegorz.jozw@gmail.com>
2023-10-27 21:21:50 +0200

    install mongoose and small refactors

--- app.js
+++ app.js
@@ -9,16 +9,15 @@ export const app = express();
 
 config();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-const env = process.env;
+const logFormat = app.get("env") === "development" ? "dev" : "tiny";
 
-app.use(logger(formatsLogger));
+app.use(logger(logFormat));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/contacts", contactsRouter);
 app.use((request, response) => {
-  response.status(404).json({ message: "Page not found" });
+  response.status(404).json({ message: "Resource not found" });
 });
 app.use((error, request, response, next) => {
   response.status(500).json({ message: error.message });
