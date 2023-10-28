f9be297d36a958e421bbae21d3b1282492afcbd6
GregorJosh <grzegorz.jozw@gmail.com>
2023-10-28 22:20:10 +0200

    add regex validation

--- models/contact.js
+++ models/contact.js
@@ -1,9 +1,24 @@
 import { mongoose, Schema } from "mongoose";
 
 const contact = new Schema({
-  name: String,
-  email: String,
-  phone: String,
+  name: {
+    type: String,
+    minLength: 3,
+    match: /^[a-zA-ZąęćłńóśźżĄĘĆŁŃÓŚŹŻ ']+$/,
+    required: true,
+  },
+  email: {
+    type: String,
+    minLength: 3,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    required: true,
+  },
+  phone: {
+    type: String,
+    minLength: 5,
+    match: /^[\d\+\-()]+$/,
+    required: true,
+  },
   favorite: Boolean,
 });
 
