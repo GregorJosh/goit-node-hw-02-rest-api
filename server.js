f3dd3d8b04105345b2e1f89fdd86bfcf5495c6ed
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-02 18:07:17 +0100

    add error controllers calls

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import ansi from "ansi-colors-es6";
 import { APIRouter } from "./api/index.js";
 import { connectToDB } from "./drivers/mongo.js";
 
-const app = express();
-const logFormat = app.get("env") === "development" ? "dev" : "tiny";
+import { notFound } from "./controllers/errors/notFound.js";
+import { internalError } from "./controllers/errors/internal.js";
 
 const start = async () => {
   const PORT = process.env.PORT || 3000;
@@ -20,28 +20,15 @@ const start = async () => {
   });
 };
 
+const app = express();
+const logFormat = app.get("env") === "development" ? "dev" : "tiny";
+
 app.use(logger(logFormat));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", APIRouter);
-app.use((request, response) => {
-  response.status(404).json({
-    status: "error",
-    code: 404,
-    message: "Use api on routes: /api/contacts",
-    data: "Not found",
-  });
-});
-app.use((error, request, response, next) => {
-  console.log(error.stack);
-
-  response.status(500).json({
-    status: "fail",
-    code: 500,
-    message: error.message,
-    data: "Internal Server Error",
-  });
-});
+app.use(notFound);
+app.use(internalError);
 
 start();
