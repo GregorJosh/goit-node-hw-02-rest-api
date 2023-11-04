e145dc35cea429f5980dcba79f678c6d40389549
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-04 15:49:52 +0100

    create user model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+import { mongoose, Schema } from "mongoose";
+
+const schema = {
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+};
+
+const user = new Schema(schema);
+
+export default mongoose.model("user", user);
