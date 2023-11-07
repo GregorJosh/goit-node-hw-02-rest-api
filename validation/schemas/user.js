240517e9b2e36b5e1604ec746681e2eec0512328
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-07 17:45:21 +0100

    add user schema

---
+++ validation/schemas/user.js
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  password: Joi.string().pattern(
+    new RegExp(`^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[-+*_]+).{8,}`)
+  ),
+});
