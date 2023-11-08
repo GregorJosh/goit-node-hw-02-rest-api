fadc58c4911c8cdfb0783b5e797ed69be40638b3
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-08 19:06:19 +0100

    add auth middleware to routes

--- api/index.js
+++ api/index.js
@@ -11,13 +11,15 @@ import {
   loginUser,
 } from "../controllers/index.js";
 
+import { authMW } from "../auth/index.js";
+
 export const APIRouter = Router();
 
-APIRouter.get("/contacts", indexContacts);
-APIRouter.get("/contacts/:id", getContact);
-APIRouter.post("/contacts", createContact);
-APIRouter.delete("/contacts/:id", removeContact);
-APIRouter.put("/contacts/:id", updateContact);
-APIRouter.patch("/contacts/:id/favorite", updateContactStatus);
+APIRouter.get("/contacts", authMW, indexContacts);
+APIRouter.get("/contacts/:id", authMW, getContact);
+APIRouter.post("/contacts", authMW, createContact);
+APIRouter.delete("/contacts/:id", authMW, removeContact);
+APIRouter.put("/contacts/:id", authMW, updateContact);
+APIRouter.patch("/contacts/:id/favorite", authMW, updateContactStatus);
 APIRouter.post("/users/signup", signupUser);
 APIRouter.post("/users/login", loginUser);
