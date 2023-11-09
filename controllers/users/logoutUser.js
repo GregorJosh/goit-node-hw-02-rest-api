acc3176edb84c327a9698d9cc77a2ac818f4f445
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-09 16:55:59 +0100

    create logoutUser controller

---
+++ controllers/users/logoutUser.js
@@ -0,0 +1,13 @@
+import User from "../../models/user.js";
+
+export const logoutUser = async (request, response, next) => {
+  try {
+    await User.findByIdAndUpdate(request.user.id, { token: null });
+
+    request.user = null;
+  } catch (error) {
+    next(error);
+  }
+
+  return response.status(204).end();
+};
