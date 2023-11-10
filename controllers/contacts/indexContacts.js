6e021f3b08f8412d7df2a9b098e0b109cb7c1e26
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-10 14:13:17 +0100

    add pagination and filtering

--- controllers/contacts/indexContacts.js
+++ controllers/contacts/indexContacts.js
@@ -1,12 +1,21 @@
 import Contact from "../../models/contact.js";
 
 export async function indexContacts(request, response, next) {
+  const { page, limit, favorite } = request.query;
+  const numOfSkipped = (page - 1) * limit;
+  const setFavorite = () => favorite && {favorite: (favorite == "true")};
+
   try {
-    const contacts = await Contact.find();
+    const numOfContacts = await Contact.count(setFavorite());
+    const contacts = await Contact.find(setFavorite())
+      .skip(numOfSkipped)
+      .limit(limit);
 
     response.status(200).json({
-      status: "success",
+      status: "Success",
       code: 200,
+      page,
+      count: numOfContacts,
       data: contacts,
     });
   } catch (error) {
