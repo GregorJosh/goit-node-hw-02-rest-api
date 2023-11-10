c9b9040264f4757352cbe14de1651fac33c8183c
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-10 14:14:30 +0100

    create updateUser controller

---
+++ controllers/users/updateUser.js
@@ -0,0 +1,30 @@
+import User from "../../models/user.js";
+import { userSchema, validate } from "../../validation/index.js";
+
+export const updateUser = async (request, response, next) => {
+  const { user, body } = request;
+  const { subscription } = body;
+  const [isValid, resultOrError] = await validate(userSchema, { subscription });
+
+  if (isValid) {
+    try {
+      const { email } = await User.findByIdAndUpdate(user.id, { subscription });
+
+      return response.status(201).json({
+        status: "Success",
+        code: 200,
+        data: { email, subscription },
+      });
+    } catch (error) {
+      next(error);
+
+      return;
+    }
+  }
+
+  return response.status(400).json({
+    status: "Bad Request",
+    code: 400,
+    message: resultOrError,
+  });
+};
