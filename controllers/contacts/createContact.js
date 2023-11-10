3aa7f468ec1b55fa6f2ebab217156ffaf329f7cb
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-10 22:24:27 +0100

    add checking ifuser name exists

--- controllers/contacts/createContact.js
+++ controllers/contacts/createContact.js
@@ -2,6 +2,20 @@ import Contact from "../../models/contact.js";
 
 export async function createContact(request, response, next) {
   try {
+    const { name } = request.body;
+    const existedContact = await Contact.findOne(
+      { name: { $regex: `^${name}$`, $options: "i" } },
+      { name: 1 }
+    ).lean();
+
+    if (existedContact) {
+      return response.status(409).json({
+        status: "Conflict",
+        code: 409,
+        message: "There is an contact with name " + name,
+      });
+    }
+
     const newContact = await Contact.create(request.body);
 
     response.status(200).json({
