fcd2e1d6c62d75415cd23958de2ec7da4e0537a1
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-20 22:22:36 +0100

    refactor controller imports

--- api/index.js
+++ api/index.js
@@ -1,38 +1,32 @@
 import { Router } from "express";
 
-import {
-  indexContacts,
-  getContact,
-  createContact,
-  updateContact,
-  removeContact,
-  updateContactStatus,
-  signupUser,
-  loginUser,
-  logoutUser,
-  currentUser,
-  updateUser,
-  updateUserAvatar,
-} from "#controllers/index.js";
-
+import * as controller from "#controllers/index.js";
 import { authMW } from "#auth/index.js";
 import { uploadMW } from "#config/storage.js";
 
 export const APIRouter = Router();
 
-APIRouter.get("/contacts", authMW, indexContacts);
-APIRouter.get("/contacts/:id", authMW, getContact);
-APIRouter.post("/contacts", authMW, createContact);
-APIRouter.delete("/contacts/:id", authMW, removeContact);
-APIRouter.put("/contacts/:id", authMW, updateContact);
-APIRouter.patch("/contacts/:id/favorite", authMW, updateContactStatus);
-APIRouter.post("/users/signup", signupUser);
-APIRouter.post("/users/login", loginUser);
-APIRouter.get("/users/logout", authMW, logoutUser);
-APIRouter.get("/users/current", authMW, currentUser);
-APIRouter.patch("/users", authMW, updateUser);
+APIRouter.get("/contacts", authMW, controller.indexContacts);
+APIRouter.get("/contacts/:id", authMW, controller.getContact);
+APIRouter.post("/contacts", authMW, controller.createContact);
+APIRouter.delete("/contacts/:id", authMW, controller.removeContact);
+APIRouter.put("/contacts/:id", authMW, controller.updateContact);
+APIRouter.patch(
+  "/contacts/:id/favorite",
+  authMW,
+  controller.updateContactStatus
+);
+
+APIRouter.post("/users/signup", controller.signupUser);
+APIRouter.post("/users/login", controller.loginUser);
+APIRouter.get("/users/logout", authMW, controller.logoutUser);
+APIRouter.get("/users/current", authMW, controller.currentUser);
+APIRouter.patch("/users", authMW, controller.updateUser);
 APIRouter.patch(
   "/users/avatars",
-  authMW, uploadMW.single("avatar"),
-  updateUserAvatar
+  authMW,
+  uploadMW.single("avatar"),
+  controller.updateUserAvatar
 );
+APIRouter.get("/users/verify/:verificationToken", controller.verifyUser);
+APIRouter.post("/users/verify", controller.reVerifyUser);
